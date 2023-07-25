2bdbd538f9590b4af23fc04e28b62f756782c486
yjhwang-cubox-ai <youngjun.hwang@cubox.ai>
2023-07-25 18:57:59 +0900

    linked list

--- C/LinkedList.c
+++ C/LinkedList.c
@@ -33,6 +33,45 @@ void appendNode(Node** head, Node* newNode){
     }
 }
 
+Node* GetNodeAt(Node* head, int Location){
+    Node* current = head;
+    while(current != NULL && (--Location) >=0){
+        current = current->next;
+    }
+    return current;
+}
+
+void RemoveNode(Node** head, Node* remove){
+    if (*head == remove){
+        *head = remove->next;
+    }
+    else{
+        Node* current = *head;
+        while(current != NULL && current->next != remove){
+            current = current->next;
+        }
+
+        if (current != NULL){
+            current->next = remove->next;
+        }
+    }
+}
+
+void insertAfter(Node* current, Node* newNode){
+    newNode->next = current->next;
+    current->next = newNode;
+}
+
+int Count(Node* node){
+    int count = 0;
+    Node* current = node;
+    while(current != NULL){
+        current = current->next;
+        count ++;
+    }
+    return count;
+}
+
 int main(){
     Node* List = NULL;
     Node* newNode = NULL;
@@ -43,6 +82,15 @@ int main(){
     appendNode(&List, newNode);
     newNode = CreateNode(3);
     appendNode(&List, newNode);
+    Node* a = GetNodeAt(List, 1);    
+    printf("%d\n", a->Data);
+    RemoveNode(&List, a);
+    Node* b = GetNodeAt(List, 0);
+    newNode = CreateNode(100);
+    insertAfter(b, newNode);
+
+    int aa = Count(List);
+    printf("%d\n",aa);
 
     return 0;
 }
