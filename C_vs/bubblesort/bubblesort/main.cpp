6fbe4f2a1b902433eb57ee592f6cc9d344677a49
yjhwang-cubox-ai <youngjun.hwang@cubox.ai>
2023-09-13 09:51:52 +0900

    퀵정렬

--- C_vs/bubblesort/bubblesort/main.cpp
+++ C_vs/bubblesort/bubblesort/main.cpp
@@ -19,12 +19,59 @@ void BubbleSort(int DataSet[], int Length)
 
 }
 
+void InsertionSort(int DataSet[], int Length)
+{
+	int i = 0;
+	int j = 0;
+	int value = 0;
+	int temp = 0;
+	for (i = 1; i < Length; i++) {
+
+		temp = DataSet[i];
+		for (j = i; j > 0 && DataSet[j - 1] > temp; j--) {
+			DataSet[j] = DataSet[j - 1];
+		}
+		DataSet[j] = temp;
+	}
+}
+
+void quick_sort(int DataSet[], int left, int right) {
+	int pL = left;
+	int pR = right;
+	int pivot = DataSet[(pL + pR) / 2];
+	int temp;
+
+	do {
+		while (DataSet[pL] < pivot) {
+			pL++;
+		}
+		while (DataSet[pR] > pivot) {
+			pR--;
+		}
+	
+		if (pL <= pR) {
+			temp = DataSet[pR];
+			DataSet[pR] = DataSet[pL];
+			DataSet[pL] = temp;
+			pL++;
+			pR--;
+		}
+	} while (pL <= pR);
+
+	if(left<pR)
+		quick_sort(DataSet, left, pR);
+	if (right > pL)
+		quick_sort(DataSet, pL, right);\
+}
+
 int main() {
 	int DataSet[] = { 6,4,2,3,1,5 };
 	int Length = sizeof DataSet / sizeof DataSet[0];
 	int i = 0;
 
-	BubbleSort(DataSet, Length);
+	//BubbleSort(DataSet, Length);
+	//InsertionSort(DataSet, Length);
+	quick_sort(DataSet, 0, Length - 1);
 
 	for (i = 0; i < Length; i++) {
 		printf("%d", DataSet[i]);
@@ -34,3 +81,5 @@ int main() {
 
 	return 0;
 }
+
+
